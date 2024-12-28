23758fe24ff911d7c96cb10d1ad1a5c8d618a5e7
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 12:26:25 +0000

    add closemocap [ci skip]

--- _news/announcement_2.md
+++
@@ -1,7 +0,0 @@
----
-layout: post
-title: EasyMocap v0.2 is released
-date: 2022-05-07 16:11:00-0400
-inline: false
----
-TODO
--- assets/js/common.js
+++ assets/js/common.js
@@ -1,15 +1,1 @@
-$(document).ready(function() {
-    $('a.abstract').click(function() {
-        $(this).parent().parent().find(".abstract.hidden").toggleClass('open');
-    });
-    $('a.qing').click(function() {
-        $(this).parent().parent().find(".qing.hidden").toggleClass('open');
-    });
-    $('a.sida').click(function() {
-        $(this).parent().parent().find(".sida.hidden").toggleClass('open');
-    });
-    $('a.bibtex').click(function() {
-        $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
-    });
-    $('a').removeClass('waves-effect waves-light');
-});
+$(document).ready(function(){$("a.abstract").click(function(){$(this).parent().parent().find(".abstract.hidden").toggleClass("open")}),$("a.qing").click(function(){$(this).parent().parent().find(".qing.hidden").toggleClass("open")}),$("a.sida").click(function(){$(this).parent().parent().find(".sida.hidden").toggleClass("open")}),$("a.bibtex").click(function(){$(this).parent().parent().find(".bibtex.hidden").toggleClass("open")}),$("a").removeClass("waves-effect waves-light")});
